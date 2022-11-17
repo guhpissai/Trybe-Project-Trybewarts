e14f22865e69d92292cba8f804731bb70cd457ca
guhpissai <gustavopissai3d@gmail.com>
2022-11-17 20:04:44 -0300

    Arthur, requisito 17, 18 e 19 concluido!

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
-const button = document.getElementById("button");
-const textarea = document.getElementById("textarea");
-const counter = document.getElementById("counter");
+const button = document.getElementById('button');
+const textarea = document.getElementById('textarea');
+const counter = document.getElementById('counter');
 
 const buttonValid = () => {
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
 
-  if (email === "tryber@teste.com" & password === "123456") {
-    window.alert("Olá, Tryber!")
+  if (email === 'tryber@teste.com' && password === '123456') {
+    window.alert('Olá, Tryber!');
   } else {
-    window.alert("Email ou senha inválidos.")
+    window.alert('Email ou senha inválidos.');
   }
-}
+};
 
-button.addEventListener("click", buttonValid);
+button.addEventListener('click', buttonValid);
 
+textarea.addEventListener('keyup', () => {
+  const caracteres = textarea.value.length;
+  const resto = 500 - caracteres;
+  counter.textContent = resto;
+});
 
-textarea.addEventListener("keyup", () => {
-  const caracteres = textarea.value.length
-  const resto = 500 - caracteres
-  counter.textContent = resto
-})
+const valid = () => {
+  const submitBtn = document.getElementById('submit-btn');
+  submitBtn.toggleAttribute('disabled');
+  console.log('ok');
+};
+
+const agreement = document.getElementById('agreement');
+agreement.addEventListener('change', valid);
