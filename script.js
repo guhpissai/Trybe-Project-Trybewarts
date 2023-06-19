725b36acc7beb18bbbe506bae2a7cd1d3a00b9cb
guhpissai <gustavopissai3d@gmail.com>
2023-06-19 16:48:43 -0300

    Implementando funcao para exibir o resultado dos inputs

--- script.js
+++ script.js
@@ -7,10 +7,9 @@ const buttonValid = () => {
   const password = document.getElementById('password').value;
 
   if (email === 'tryber@teste.com' && password === '123456') {
-    window.alert('Olá, Tryber!');
-  } else {
-    window.alert('Email ou senha inválidos.');
+    return window.alert('Olá, Tryber!');
   }
+  return window.alert('Email ou senha inválidos.');
 };
 
 button.addEventListener('click', buttonValid);
@@ -24,8 +23,63 @@ textarea.addEventListener('keyup', () => {
 const valid = () => {
   const submitBtn = document.getElementById('submit-btn');
   submitBtn.toggleAttribute('disabled');
-  console.log('ok');
 };
 
 const agreement = document.getElementById('agreement');
 agreement.addEventListener('change', valid);
+
+// Submit form
+
+const submitBtn = document.getElementById('submit-btn');
+
+let familyResult = '';
+let avaliation = '';
+const contentArray = [];
+const nameValue = document.getElementById('input-name').value;
+const lastName = document.getElementById('input-lastname').value;
+const house = document.getElementById('house').value;
+const family = document.querySelectorAll('input[name=family]');
+const content = document.querySelectorAll('.subject');
+const avaliationForm = document.querySelectorAll('input[name=rate]');
+const commentaryInput = document.getElementById('textarea');
+
+const checkedFamily = () => {
+  for (let i = 0; i < family.length; i += 1) {
+    if (family[i].checked) {
+      familyResult = family[i].value;
+    }
+  }
+};
+
+const checkedContent = () => {
+  for (let i = 0; i < content.length; i += 1) {
+    if (content[i].checked) {
+      contentArray.push(content[i].value);
+    }
+  }
+};
+
+const checkedAvaliation = () => {
+  for (let i = 0; i < avaliationForm.length; i += 1) {
+    if (avaliationForm[i].checked) {
+      avaliation = avaliationForm[i].value;
+    }
+  }
+};
+
+submitBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  checkedContent();
+  checkedFamily();
+  checkedAvaliation();
+
+  document.querySelector('.name').innerHTML = `${nameValue} ${lastName}`;
+  document.querySelector('.house').innerHTML = `Casa: ${house}`;
+  document.querySelector('.family').innerHTML = `Familia: ${familyResult}`;
+  document.querySelector('.content').innerHTML = `Conteudo: ${contentArray.join(', ')}`;
+  document.querySelector('.avaliation').innerHTML = `Avaliação: ${avaliation}`;
+  document.querySelector('.commentary').innerText = `Comentario: ${commentaryInput.value}`;
+
+  document.getElementById('evaluation-form').style.display = 'none';
+});
